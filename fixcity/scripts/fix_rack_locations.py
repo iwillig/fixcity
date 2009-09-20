04b3d53eb160bba0a58df0bf1ac4a839ff91b710
Paul Winkler <slinkp@gmail.com>
2009-09-19 22:59:40 -0400

    fix_rack_locations.py: tolerate no results from geocode

--- fixcity/scripts/fix_rack_locations.py
+++ fixcity/scripts/fix_rack_locations.py
@@ -15,7 +15,12 @@ from fixcity.bmabr.models import Rack
 def fix():
     for rack in Rack.objects.all():
         newloc = [x for x in geocoder.geocode(rack.address, exactly_one=False)]
-        newloc = newloc[0]
+        try:
+            newloc = newloc[0]
+        except IndexError:
+            print "Got no results"
+            print
+            continue
         if newloc[1] != (rack.location.y, rack.location.x):
             rack.location = 'POINT(%f %f)' % (newloc[1][1], newloc[1][0])
             print "fixed", rack.title, rack.address, rack.location
