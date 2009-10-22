e3f1d0e7dde31fb87b0b5d4037acde8285c257e1
Paul Winkler <slinkp@gmail.com>
2009-10-22 16:18:20 -0400

    Fix yet more js bugs reported by js2-mode

--- fixcity/media/js/app.js
+++ fixcity/media/js/app.js
@@ -14,41 +14,41 @@ jQuery.fn.infoSuffixify = function() { //chainable
 		var obj = jQuery(this);
 		jQuery('<img src="/site_media/img/info.png" alt="More information about this field" title="' + obj.attr('title') + '" />').click(function () {return false;}).appendTo('label[for="' + obj.attr('id') + '"]');
 	});
-}
+};
 
 // Taken from http://www.andrewpeace.com/textarea-maxlength.html
 jQuery.fn.truncate = function(len)
 {
   this.val(this.val().substring(0,len));
   return false;
-}
+};
 
 jQuery.fn.maxLength = function(len)
 {
   var maxLengthKeyPress = new Array();
   var maxLengthChange = new Array();
   var appleKeyOn = false;
-  
+
   //the second argument should be true if len should be based on
   //the maxlength attribute instead of the input
   var useAttr = arguments.length>1 ? arguments[1] : false;
-  
+
   var handleKeyUp = function(e)
   {
     //if the apple key (macs) is being pressed, set the indicator
     if(e.keyCode==224||e.keyCode==91)
       appleKeyOn = false;
-  }
-  
+  };
+
   var handleKeyDown = function(e)
   {
     //if the apple key (macs) is being released, turn off the indicator
     if(e.keyCode==224||e.keyCode==91)
       appleKeyOn = true;
-  }
-  
+  };
+
   var handleKeyPress = function(e)
-  {   
+  {
     //if this keyCode does not increase the length of the textarea value,
     //just let it go
     if(appleKeyOn || (e.charCode==0&&e.keyCode!=13) || e.ctrlKey)
@@ -70,20 +70,20 @@ jQuery.fn.maxLength = function(len)
     else if(val.length>len && selected<(val.length-len))
       return textarea.truncate(len);
   };
-  
+
   var handleChange = function(e)
   {
     //get the textarea element
     var textarea = jQuery(this);
-    
+
     //if the length should be based on the maxlength attribute instead of the
     //input, use that
     len = useAttr ? parseInt(textarea.attr('maxlength')) : len;
-    
+
     //truncate the textarea to its proper length
     textarea.truncate(len);
   };
-  
+
   //get the current keyup and change functions
   var removeKeyPress = maxLengthKeyPress[this.selector];
   var removeChange = maxLengthChange[this.selector];
@@ -92,22 +92,22 @@ jQuery.fn.maxLength = function(len)
   //a maxlength was previously set and a new one is being set
   this.die('keypress', removeKeyPress);
   this.die('change', removeChange);
-  
+
   if(len==0 && !useAttr)
     return;
-  
+
   //set the keyup and change functions for this element set and all future
   //elements matching this selector
   this.live('keypress', handleKeyPress);
   this.live('change', handleChange);
   this.live('keydown', handleKeyDown);
   this.live('keyup', handleKeyUp);
-  
+
   //save the current keyup and change functions so that they can be
   //remove later
   maxLengthKeyPress[this.selector] = handleKeyPress;
   maxLengthChange[this.selector] = handleChange;
-  
+
   //trigger a keypress event so that the limit will be enforced
   this.keypress();
 };
